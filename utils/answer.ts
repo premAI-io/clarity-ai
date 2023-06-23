18423606c7872d73929e320bcd03e695ed866f37
tiero <3596602+tiero@users.noreply.github.com>
2023-06-23 15:56:03 +0200

    chore: cleanup

--- utils/answer.ts
+++ utils/answer.ts
@@ -5,7 +5,7 @@ export const OpenAIStream = async (prompt: string, apiKey: string) => {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/chat/completions`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/chat/completions`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${apiKey}`
@@ -28,35 +28,6 @@ export const OpenAIStream = async (prompt: string, apiKey: string) => {
   }
 
   const json = await res.json();
-  /* const stream = new ReadableStream({
-    async start(controller) {
-      const onParse = (event: ParsedEvent | ReconnectInterval) => {
-        if (event.type === "event") {
-          const data = event.data;
-
-          if (data === "[DONE]") {
-            controller.close();
-            return;
-          }
-
-          try {
-            const json = JSON.parse(data);
-            const text = json.choices[0].delta.content;
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
-          } catch (e) {
-            controller.error(e);
-          }
-        }
-      };
-
-      const parser = createParser(onParse);
-
-      for await (const chunk of res.body as any) {
-        parser.feed(decoder.decode(chunk));
-      }
-    }
-  }); */
   const [choice] = json.choices;
   return choice.message.content;
 };
